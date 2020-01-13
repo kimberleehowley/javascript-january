62f62148dc10f05884b5f2455ec7146615ceb865
Kimberlee Johnson <kimee.johnson@gmail.com>
2020-01-12 19:54:01 -0800

    Updated buttons to toggle on and off

--- javscript30/11_custom_video/scripts.js
+++ javscript30/11_custom_video/scripts.js
@@ -14,5 +14,17 @@ function togglePlay() {
     video[method](); 
 }
 
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚'; 
+    console.log(icon); 
+    toggle.textContent = icon;
+}
+
 // Hook up event listeners
 video.addEventListener('click', togglePlay); 
+video.addEventListener('play', updateButton); 
+video.addEventListener('pause', updateButton); 
+
+toggle.addEventListener('click', togglePlay); 
+
+// Listen for when event toggles
